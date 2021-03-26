707e142ad7ee4d6c30d4d2a2d80c322e09d22e35
Adam Torkkeli <adam.torkkeli@getaccept.com>
2021-03-26 10:50:45 +0100

    server: add CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,10 @@
 const QueueController = require("./controllers/queue.controller");
-
 const express = require("express");
+var cors = require("cors");
+
 const app = express();
+app.use(cors());
+
 const port = 3000;
 
 app.use("/queues", QueueController);
